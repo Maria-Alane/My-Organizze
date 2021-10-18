c713b69bead627000b0c631aaf312125e7d7040c
maria-alane <maria.alane.bs@gmail.com>
2021-10-18 11:01:40 -0300

    interface de cadastro e login

--- app/src/main/java/com/chaveirinho/myorganizze/MainActivity.java
+++ app/src/main/java/com/chaveirinho/myorganizze/MainActivity.java
@@ -2,8 +2,12 @@ package com.chaveirinho.myorganizze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.chaveirinho.myorganizze.activity.CadastroActivity;
+import com.chaveirinho.myorganizze.activity.LoginActivity;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -41,5 +45,21 @@ public class MainActivity extends IntroActivity {
                 .build()
         );
 
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoBackward(true)
+                .canGoForward(false)
+                .build()
+        );
+
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
