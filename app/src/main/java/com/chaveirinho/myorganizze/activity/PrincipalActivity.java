de2b8aeaf6cccfac28effc43b58aa1c903beb8a0
maria-alane <maria.alane.bs@gmail.com>
2021-10-26 10:22:45 -0300

    configurando toolbar, deslogando usuario

--- app/src/main/java/com/chaveirinho/myorganizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/chaveirinho/myorganizze/activity/PrincipalActivity.java
@@ -3,10 +3,18 @@ package com.chaveirinho.myorganizze.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.chaveirinho.myorganizze.config.ConfiguracaoFirebase;
 import com.chaveirinho.myorganizze.databinding.ActivityPrincipalBinding;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toolbar;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,11 +22,13 @@ import androidx.navigation.ui.NavigationUI;
 
 
 import com.chaveirinho.myorganizze.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +36,8 @@ public class PrincipalActivity extends AppCompatActivity {
 
      binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
      setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
+    binding.toolbar.setTitle("Organizze");
+    setSupportActionBar(binding.toolbar);
 
         /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -40,6 +50,25 @@ public class PrincipalActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarReceita(View view){
